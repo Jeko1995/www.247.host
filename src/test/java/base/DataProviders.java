759a4767e8329a7e3592b82b34fcd4b262e4b35b
Jeko1995 <jp.shahanov@gmail.com>
2025-03-10 19:06:22 +0200

    - Optimize DataProviders.java

--- src/test/java/base/DataProviders.java
+++ src/test/java/base/DataProviders.java
@@ -6,142 +6,60 @@ import org.testng.annotations.DataProvider;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class DataProviders {
 
-    //Data provider for correct login credentials.
+    // This method reads a CSV file from the given file path and converts the rows into an array
+    private static Object[][] readCsvData(String filePath, int columnCount) throws IOException, CsvException {
+        try (CSVReader csvReader = new CSVReader(new FileReader(filePath))) {
+            List<String[]> csvData = csvReader.readAll();
+
+            // Skip lines starting with // (comments)
+            List<String[]> validRows = csvData.stream()
+                    .filter(row -> row.length > 0 && !row[0].startsWith("//")).toList();
+
+            // Transfer to Object[][]
+            Object[][] csvDataObj = new Object[validRows.size()][columnCount];
+            for (int i = 0; i < validRows.size(); i++) {
+                csvDataObj[i] = validRows.get(i);
+            }
+            return csvDataObj;
+        }
+    }
+
+    //Data provider for correct login credentials
     @DataProvider(name = "correctLoginCredentials")
     public Object[][] readCorrectLoginCredentials() throws IOException, CsvException {
-        try {
-            CSVReader csvReader = new CSVReader(new FileReader("src/test/resources/correctLoginCredentials.csv"));
-            List<String[]> csvData = csvReader.readAll();
-            List<String[]> validRows = new ArrayList<>();
-
-            for (String[] row : csvData) {
-                if (!row[0].startsWith("//")) {
-                    validRows.add(row);
-                }
-            }
-
-            Object[][] csvDataObj = new Object[validRows.size()][2];
-
-            for (int i = 0; i < validRows.size(); i++) {
-                csvDataObj[i] = validRows.get(i);
-            }
-
-            return csvDataObj;
-
-        } catch (IOException | CsvException e) {
-            throw (e);
-        }
+        return readCsvData("src/test/resources/correctLoginCredentials.csv", 2);
     }
 
-    //Data provider for invalid login credentials.
+    //Data provider for invalid login credentials
     @DataProvider(name = "invalidLoginCredentials")
     public Object[][] readInvalidLoginCredentials() throws IOException, CsvException {
-        try {
-            CSVReader csvReader = new CSVReader(new FileReader("src/test/resources/invalidLoginCredentials.csv"));
-            List<String[]> csvData = csvReader.readAll();
-            List<String[]> validRows = new ArrayList<>();
-
-            for (String[] row : csvData) {
-                if (!row[0].startsWith("//")) {
-                    validRows.add(row);
-                }
-            }
-
-            Object[][] csvDataObj = new Object[validRows.size()][2];
-            for (int i = 0; i < validRows.size(); i++) {
-                csvDataObj[i] = validRows.get(i);
-            }
-
-            return csvDataObj;
-
-        } catch (IOException | CsvException e) {
-            throw (e);
-        }
+        return readCsvData("src/test/resources/invalidLoginCredentials.csv", 2);
     }
 
-    //Data provider for getting correct email credentials of the user.
+    //Data provider for getting correct email credentials of the user
     @DataProvider(name = "getEmail")
     public Object[][] readCorrectEmails() throws IOException, CsvException {
-        try {
-            CSVReader csvReader = new CSVReader(new FileReader("src/test/resources/correctLoginCredentials.csv"));
-            List<String[]> csvData = csvReader.readAll();
-            List<String[]> validRows = new ArrayList<>();
-
-            for (String[] row : csvData) {
-                if (!row[0].startsWith("//")) {
-                    validRows.add(row);
-                }
-            }
-
-            Object[][] csvDataObj = new Object[validRows.size()][1];
-
-            for (int i = 0; i < validRows.size(); i++) {
-                csvDataObj[i][0] = validRows.get(i)[0].trim();
-            }
-
-            return csvDataObj;
-
-        } catch (IOException | CsvException e) {
-            throw (e);
+        Object[][] data = readCsvData("src/test/resources/correctLoginCredentials.csv", 2);
+        Object[][] emails = new Object[data.length][1];
+        for (int i = 0; i < data.length; i++) {
+            emails[i][0] = data[i][0].toString().trim();
         }
+        return emails;
     }
 
-    //Data provider for invalid registration credentials.
+    //Data provider for invalid registration credentials
     @DataProvider(name = "invalidRegistrationCredentials")
     public Object[][] readInvalidRegistrationCredentials() throws IOException, CsvException {
-        try {
-            CSVReader csvReader = new CSVReader(new FileReader("src/test/resources/invalidRegistrationCredentials.csv"));
-            List<String[]> csvData = csvReader.readAll();
-            List<String[]> validRows = new ArrayList<>();
-
-            for (String[] row : csvData) {
-                if (!row[0].startsWith("//")) {
-                    validRows.add(row);
-                }
-            }
-
-            Object[][] csvDataObj = new Object[validRows.size()][8];
-
-            for (int i = 0; i < validRows.size(); i++) {
-                csvDataObj[i] = validRows.get(i);
-            }
-
-            return csvDataObj;
-
-        } catch (IOException | CsvException e) {
-            throw (e);
-        }
+        return readCsvData("src/test/resources/invalidRegistrationCredentials.csv", 8);
     }
 
-    //Data provider for correct registration credentials.
+    //Data provider for correct registration credentials
     @DataProvider(name = "correctRegistrationCredentials")
     public Object[][] readCorrectRegistrationCredentials() throws IOException, CsvException {
-        try {
-            CSVReader csvReader = new CSVReader(new FileReader("src/test/resources/correctRegistrationCredentials.csv"));
-            List<String[]> csvData = csvReader.readAll();
-            List<String[]> validRows = new ArrayList<>();
-
-            for (String[] row : csvData) {
-                if (!row[0].startsWith("//")) {
-                    validRows.add(row);
-                }
-            }
-
-            Object[][] csvDataObj = new Object[validRows.size()][8];
-
-            for (int i = 0; i < validRows.size(); i++) {
-                csvDataObj[i] = validRows.get(i);
-            }
-
-            return csvDataObj;
-
-        } catch (IOException | CsvException e) {
-            throw (e);
-        }
+        return readCsvData("src/test/resources/correctRegistrationCredentials.csv", 8);
     }
 }
